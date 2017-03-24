394407057e7b93f6497719d652b5f239433133fa
jianhua-ling <jianhua_ling@163.com>
2017-03-24 15:42:16 +0800

    s

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    // require it at the top and pass in the grunt instance
+    require('time-grunt')(grunt);
+
+    // 加载包含所有任务的插件。
+    require('load-grunt-tasks')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        copy : {
+            init : {
+                files:{
+                    '../':'**'
+                }
+            }
+        },
+        clean : {
+            init : ['../grunt-mobile']
+        }
+    });
+
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['uglify']);
+
+};
